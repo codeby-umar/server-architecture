b75ad87f7d5973abb66ec1f59933420a07b36f7f
codeby-umar <codingbyumar@gmail.com>
2026-03-17 09:20:00 +0500

    change

--- index.js
+++ index.js
@@ -152,3 +152,74 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000);
 console.log(`Sizni portingiz ${server.address().port} ishlashni boshladim ...`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const https = require("http");
+
+// const server = https.createServer((req , res)=>{
+
+//      const users = [
+//         {
+//              id : 1,
+//              age : 17,
+//              name : "Muhamad Umar",
+//              login : "admin@gmail.com",
+//              password : 1232343,
+//              phone_Number : 937981208
+//         },
+//         {
+//              id : 2,
+//              age : 17,
+//              name : "Muhamad Yusuf",
+//              login : "yusuf@gmail.com",
+//              password : 1232343,
+//              phone_Number : 937981208
+//         },
+//         {
+//              id : 3,
+//              age : 17,
+//              name : "Siddiq",
+//              login : "siddiq@gmail.com",
+//              password : 1232343,
+//              phone_Number : 937981208
+//         },
+//         {
+//              id : 4,
+//              age : 17,
+//              name : "Shukurullo",
+//              login : "shukurullo@gmail.com",
+//              password : 1232343,
+//              phone_Number : 937981208
+//         },
+//      ]
+
+//      if(req.url === "/"){
+//         res.write("Assalomu Allaykum bizni birinchi web serverimiz ishga tushdi ..");
+//         res.end();
+//      }
+//      if(req.url == "/login"){
+//         res.write(JSON.stringify(users));
+//         res.end();
+//      }
+// })
+
+// server.listen(8000);
+// console.log(`Bizni website porti : ${server.address().port} shu portda ishlayapti....`)
+
+
+
+
+
